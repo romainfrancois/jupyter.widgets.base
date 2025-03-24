84c79ce7d9174c4e7fbf6bbc67d0422b5b5b71d4
Romain Francois <romain@tada.science>
2025-03-24 17:56:10 +0100

    using dplyr

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,9 @@
 #' @import hera
 #' @import assertthat
 #' @import jsonlite
-#' @importFrom rlang current_env check_dots_empty caller_env arg_match is_true list2 caller_arg
-#' @importFrom glue glue
+#' @import dplyr
+#' @importFrom rlang current_env check_dots_empty caller_env arg_match is_true list2 caller_arg `%||%`
+#' @importFrom glue glue glue_collapse
 #' @importFrom cli cli_abort
 NULL
 
